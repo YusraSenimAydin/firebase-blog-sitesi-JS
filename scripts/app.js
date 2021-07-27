cbaa49b0b8ef7cfbb2bb82da5b24b90eebf16a89
YusraSenimDede <50294199+YusraSenimDede@users.noreply.github.com>
2021-07-27 18:14:29 +0300

    kullancı detayları-makale yukleme-giris islemi

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,63 @@
-document.addEventListener("DOMContentLoaded", function() {
+const makaleler = document.querySelector(".guides");
+
+
+const cikisLinkleri=document.querySelectorAll(".logged-out");
+const girisLinkleri=document.querySelectorAll(".logged-in");
+
+const uyelikDetaylari=document.querySelector('.account-details');
+
+const kullaniciYukle=(kullanici)=>{
+    if(kullanici){
+        let html =`
+        <div>Kullanıcı Mail: ${kullanici.email}</div>
+        `;
+        uyelikDetaylari.innerHTML=html;
+        girisLinkleri.forEach(item=>item.style.display='block');
+        cikisLinkleri.forEach(item=>item.style.display='none');     
+
+    }else{
+        uyelikDetaylari.innerHTML='';
+        girisLinkleri.forEach(item=>item.style.display='none'); 
+        cikisLinkleri.forEach(item=>item.style.display='block');     
+
+    }
+
+}
+
+
+
+
+const makaleYukle = (data) => {
+    if (data.length) {
+        let html = '';
+        data.forEach(doc => {
+            const makale = doc.data();
+            //console.log(makale);
+            const li = `
+       <li>
+       <div class="collapsible-header grey lighten-3">${makale.baslik}</div>
+       <div class="collapsible-body white"><span>${makale.icerik}</span></div>
+       </li>       
+       `;
+
+            html += li;
+
+        });
+
+        makaleler.innerHTML = html;
+    }else{
+        makaleler.innerHTML =`<h4 class="center-align">Makaleler için giriş yapınız..</h4>`;
+
+    }
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
     var modals = document.querySelectorAll(".modal");
     M.Modal.init(modals);
 
-    var makaleler= document.querySelectorAll(".collapsible");
+    var makaleler = document.querySelectorAll(".collapsible");
     M.Collapsible.init(makaleler);
 
 });
--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,41 @@
+//auth durumu izle
+
+auth.onAuthStateChanged(k=>{
+    if(k){
+        console.log("Giriş işlemi başarılı");
+        db.collection('makaleler').onSnapshot(snapshot=>{
+           // console.log(snapshot);
+           makaleYukle(snapshot.docs);
+           kullaniciYukle(k);
+        })
+    }else{
+        console.log("Çıkış işlemi başarılı");
+        makaleYukle([]);
+        kullaniciYukle();
+    }
+})
+
+
+//makale oluştur 
+ const makaleOlusturForm = document.querySelector('#create-form');
+
+ makaleOlusturForm.addEventListener('submit',(e)=>{
+     e.preventDefault();
+
+     db.collection('makaleler').add({
+         baslik:makaleOlusturForm["title"].value,
+         icerik:makaleOlusturForm["content"].value
+     }).then(()=>{
+        const modal = document.querySelector("#modal-create");
+        M.Modal.getInstance(modal).close();
+        uyelikForm.reset();
+     }).catch(err=>{
+         console.log(err.message);
+     })
+
+ })
+
+
 //üyelik Oluştur
 
 const uyelikForm = document.querySelector("#signup-form");
@@ -24,6 +62,28 @@ const cikis=document.querySelector("#logout");
 cikis.addEventListener("click", (e)=>{
     e.preventDefault();
     auth.signOut().then(()=>{
-        console.log("Çıkış işlemi başarılı");
+        //console.log("Çıkış işlemi başarılı");
     });
 });
+
+//giriş işlemi
+
+const loginForm = document.querySelector("#login-form");
+loginForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+
+    const mail = loginForm["login-email"].value;
+    const parola=loginForm["login-password"].value;
+
+    auth.signInWithEmailAndPassword(mail, parola).then((sonuc)=>{
+         console.log(sonuc.user);
+
+          const modal = document.querySelector("#modal-login");
+           M.Modal.getInstance(modal).close();
+
+           loginForm.reset();
+    }).catch((err)=>{
+        console.log(err);
+    })
+
+})
